1e4ee8ee881a9ba87eb577e1047831d2bc253c4d
Josh Turgeon <j_turgeon88@yahoo.com>
2017-10-11 23:02:32 -0700

    Finished octopus problems

--- W2D4/octopus_problems/big_octopus.rb
+++ W2D4/octopus_problems/big_octopus.rb
@@ -59,4 +59,29 @@ def clever_o(array)
   array.each do |fish|
     longest_fish = fish if fish.length > longest_fish.length
   end
+  return longest_fish
+end
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(direction, tiles_array)
+  array.each_with_index do |dir, tentacle|
+    return tentacle if dir == direction
+  end
+end
+
+tiles_hash = {
+    "up" => 0,
+    "right-up" => 1,
+    "right"=> 2,
+    "right-down" => 3,
+    "down" => 4,
+    "left-down" => 5,
+    "left" => 6,
+    "left-up" => 7
+    }
+
+def fast_dance(direction, tiles_hash)
+  tiles_hash[direction]
+
 end
