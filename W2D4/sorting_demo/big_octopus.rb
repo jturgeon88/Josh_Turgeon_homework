da53e747efd2136d29efea08071d90eeb223ebf2
Josh Turgeon <j_turgeon88@yahoo.com>
2017-10-11 22:39:52 -0700

    Wrote and tested the first two big octopus problems

---
+++ W2D4/sorting_demo/big_octopus.rb
@@ -0,0 +1,62 @@
+fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+# O(n^2) time
+def sluggish_o(fishes)
+  longest_fish = []
+  fishes.each_index do |idx1|
+    fishes.each_index do |idx2|
+      next if idx1 == idx2
+      if fishes[idx1].length > fishes[idx2].length
+        longest_fish << fishes[idx1] unless fishes[idx1] < longest_fish[0].length
+      else
+        longest_fish << fishes[idx2] unless fishes[idx2] < longest_fish[0].length
+      end
+    end
+  end
+  longest_fish[0]
+end
+
+#O(n log n) time
+def merge_sort(array, &prc)
+  return array if array.length <= 1
+
+  mid = array.length / 2
+  left_sorted = merge_sort(array.take(mid), &prc)
+  right_sorted = merge_sort(array.drop(mid), &prc)
+
+  merge(left_sorted, right_sorted, &prc)
+
+end
+
+def merge(left, right, &prc)
+  merged = []
+  prc ||= Proc.new {|x, y| x.length <=> y.length}
+
+  until left.empty? || right.empty?
+    case prc.call(left.first, right.first)
+    when -1
+      merged << left.shift
+    when 0
+      merged << left.shift
+    when 1
+      merged << right.shift
+    end
+  end
+
+    merged.concat(left)
+    merged.concat(right)
+    merged
+
+end
+
+def dominant_o(fishes)
+  merge_sort(fishes).last
+
+end
+
+def clever_o(array)
+  longest_fish = ""
+  array.each do |fish|
+    longest_fish = fish if fish.length > longest_fish.length
+  end
+end
