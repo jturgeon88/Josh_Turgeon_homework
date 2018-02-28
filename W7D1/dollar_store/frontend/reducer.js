43631f8c7bd84eb0e7ff183955da1864fe43268c
Josh Turgeon <j_turgeon88@yahoo.com>
2018-02-27 22:18:53 -0500

    completed dollar_store hw

--- W7D1/dollar_store/frontend/reducer.js
+++ W7D1/dollar_store/frontend/reducer.js
@@ -1,10 +1,22 @@
+
+
 const initialState = {
   baseCurrency: "Please select",
   rates: {}
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  Object.freeze(state);
+  switch(action.type) {
+    case "SWITCH_CURRENCY":
+      let nextState = {
+        baseCurrency: action.baseCurrency,
+        rates: action.rates
+      }
+      return nextState;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
