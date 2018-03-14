a3c2da6e52521ae22c5a087a4d8f56ef385eb734
Josh Turgeon <j_turgeon88@yahoo.com>
2018-03-14 11:33:21 -0500

    W7D3 Folder added, intro to middleware assignment started - Phase 1 complete

---
+++ W7D3/middleware_intro/frontend/todo_redux.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+const addLoggingToDispatch = (store) => {
+  const disp = store.dispatch;
+  return (action) => {
+    console.log(store.getState());
+    console.log(action);
+    disp(action);
+    console.log(store.getState());
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+
+  //Phase 1: Logging
+  store.dispatch = addLoggingToDispatch(store);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
