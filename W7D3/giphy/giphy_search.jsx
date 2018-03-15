333738cc8c66ea2f47453d1e90db027b01b2cb88
Josh Turgeon <j_turgeon88@yahoo.com>
2018-03-15 12:06:20 -0500

    Finished store, started Thunk Middleware

--- W7D3/giphy/giphy_search.jsx
+++ W7D3/giphy/giphy_search.jsx
@@ -2,3 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import * as APIUtil from './util/api_util';
+import * as GiphyActions from './actions/giphy_actions';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
+  window.fetchSearchGiphys = APIUtil.fetchSearchGiphys;
+  window.receiveSearchGiphys = GiphyActions.receiveSearchGiphys;
+});
